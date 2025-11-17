d03ef76196da6fe3db23c36f7ccb88f37ffb1a1f
Junhuan Zheng <3373484735@qq.com>
2025-11-17 18:21:28 +0800

    feat: 责任链抽象模板设计

---
+++ group-buy-market-app/src/test/java/cn/bugstack/test/types/rule02/logic/XxxResponse.java
@@ -0,0 +1,15 @@
+package cn.bugstack.test.types.rule02.logic;
+
+public class XxxResponse {
+
+    private final String age;
+
+    public XxxResponse(String age) {
+        this.age = age;
+    }
+
+    public String getAge() {
+        return age;
+    }
+
+}
---
+++ group-buy-market-types/src/main/java/cn/bugstack/types/design/framework/link/model2/handler/ILogicHandler.java
@@ -0,0 +1,14 @@
+package cn.bugstack.types.design.framework.link.model2.handler;
+
+/**
+ *
+ *  逻辑处理器 */
+public interface ILogicHandler<T, D, R> {
+
+    default R next(T requestParameter, D dynamicContext) {
+        return null;
+    }
+
+    R apply(T requestParameter, D dynamicContext) throws Exception;
+
+}
